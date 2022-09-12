f53980ead7698ccd9b3558ee2c1b26d68fd3453d
Yoshihiko Yamanaka <bamchoh@gmail.com>
2022-09-12 18:35:18 +0900

    -

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,74 +1,30 @@
-use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::io::{BufReader, Read};
 use std::time::Instant;
 
+#[derive(Debug, Clone)]
+struct State {
+    name: String,
+    class: String,
+}
+
 #[derive(Debug)]
-struct Config<'a> {
-    states: HashMap<&'a str, State<'a>>,
-    general: &'a State<'a>,
-    soldier: &'a State<'a>,
-    firing: &'a State<'a>,
-    external: &'a State<'a>,
-    rules: HashMap<&'a str, HashMap<&'a str, HashMap<&'a str, &'a str>>>,
+struct Config {
+    states: Vec<State>,
+    general: usize,
+    soldier: usize,
+    firing: usize,
+    external: usize,
+    rules: [usize; 4096],
 }
 
-struct Env<'a> {
-    config: &'a Config<'a>,
-}
-
-impl<'a> Env<'a> {
-    fn dump(&self, current: &Vec<&'a State<'a>>) {
-        print!("|");
-        for i in 1..current.len() - 1 {
-            print!("{0: <4}|", current[i].name);
-        }
-        println!();
+fn dump(current: &Vec<usize>, config: &Config) {
+    print!("|");
+    for i in 1..current.len() - 1 {
+        print!("{0: <4}|", config.states[current[i]].name);
     }
-
-    fn next_lines(&mut self, current: &Vec<&'a State<'a>>, next_cells: &mut Vec<&'a State<'a>>) {
-        for i in 1..current.len() - 1 {
-            let left = current[i - 1].name;
-            let center = current[i].name;
-            let right = current[i + 1].name;
-
-            let l_map = &self.config.rules;
-
-            match l_map.get(left) {
-                Some(c_map) => match c_map.get(center) {
-                    Some(r_map) => match r_map.get(right) {
-                        Some(next) => {
-                            let next_state = &self.config.states[next];
-                            next_cells[i] = next_state;
-                        }
-                        None => {}
-                    },
-                    None => {}
-                },
-                None => {}
-            }
-        }
-    }
-}
-
-const EMPTY_STATE: &State = &State {
-    name: "",
-    class: "",
-};
-
-#[derive(Debug)]
-struct State<'a> {
-    name: &'a str,
-    class: &'a str,
-}
-
-#[derive(Debug)]
-struct Rule<'a> {
-    left: &'a str,
-    center: &'a str,
-    right: &'a str,
-    next: &'a str,
+    println!();
 }
 
 #[derive(Debug)]
@@ -76,6 +32,28 @@ enum ParseState {
     Begin,
     FoundState(i32),
     FoundRule(i32),
+}
+
+fn nextline<'a>(current: &Vec<usize>, config: &'a Config) -> Vec<usize> {
+    let mut next_cells = new_line(current.len() - 2, &config);
+    let mut left: usize = 0;
+    for (i, center) in current.iter().enumerate() {
+        if i == 0 {
+            left = *center;
+            continue;
+        } else if i == current.len() - 1 {
+            continue;
+        }
+
+        let right = current[i + 1];
+
+        let next = config.rules[(left << 8) + (center << 4) + right];
+        next_cells[i] = next;
+
+        left = *center;
+    }
+
+    next_cells
 }
 
 fn parse_begin(line: &str) -> ParseState {
@@ -92,11 +70,11 @@ fn parse_begin(line: &str) -> ParseState {
 }
 
 fn parse_states(line: &str, n: i32) -> (ParseState, State) {
-    let v: Vec<&str> = line.split(&[' ', ','][..]).filter(|&x| x != "").collect();
+    let v: Vec<&str> = line.split(&['@', ','][..]).collect();
 
     let state = State {
-        name: v[0],
-        class: v[3],
+        name: v[0].to_string(),
+        class: v[3].to_string(),
     };
 
     if n == 1 {
@@ -106,35 +84,46 @@ fn parse_states(line: &str, n: i32) -> (ParseState, State) {
     }
 }
 
-fn parse_rules(line: &str, n: i32) -> (ParseState, Rule) {
-    let v: Vec<&str> = line
-        .split(&[' ', '#', '-', '>'][..])
-        .filter(|&x| x != "")
-        .collect();
+fn parse_rules(line: &str, n: i32, states: &Vec<State>) -> (ParseState, usize, usize) {
+    let v: Vec<&str> = line.split("->").collect();
 
-    let rule = Rule {
-        left: v[0],
-        center: v[1],
-        right: v[2],
-        next: v[3],
-    };
+    let vv: Vec<&str> = v[0].split("##").collect();
+
+    let mut idx: usize = 0;
+
+    for rule_state in vv {
+        for (i, state) in states.iter().enumerate() {
+            if rule_state == state.name.as_str() {
+                idx = (idx << 4) + i;
+                break;
+            }
+        }
+    }
+
+    let mut n_idx: usize = 0;
+    for (i, state) in states.iter().enumerate() {
+        if v[1] == state.name.as_str() {
+            n_idx = i;
+            break;
+        }
+    }
 
     if n == 1 {
-        (ParseState::Begin, rule)
+        (ParseState::Begin, idx, n_idx)
     } else {
-        (ParseState::FoundRule(n - 1), rule)
+        (ParseState::FoundRule(n - 1), idx, n_idx)
     }
 }
 
 fn parse_rule_file(s: &String) -> Config {
     let mut parse_state = ParseState::Begin;
     let mut config = Config {
-        states: HashMap::new(),
-        rules: HashMap::new(),
-        general: EMPTY_STATE,
-        soldier: EMPTY_STATE,
-        firing: EMPTY_STATE,
-        external: EMPTY_STATE,
+        states: vec![],
+        rules: [0; 4096],
+        general: 0,
+        soldier: 0,
+        firing: 0,
+        external: 0,
     };
 
     for line in s.lines() {
@@ -142,25 +131,13 @@ fn parse_rule_file(s: &String) -> Config {
             ParseState::Begin => parse_begin(&line),
             ParseState::FoundState(n) => {
                 let (next_state, state) = parse_states(&line, n);
-                config.states.insert(state.name, state);
+                config.states.push(state);
                 next_state
             }
             ParseState::FoundRule(n) => {
-                let (next_state, rule) = parse_rules(&line, n);
+                let (next_state, i, j) = parse_rules(&line, n, &config.states);
 
-                if !config.rules.contains_key(rule.left) {
-                    config.rules.insert(rule.left, HashMap::new());
-                }
-
-                let center = config.rules.get_mut(rule.left).unwrap();
-                if !center.contains_key(rule.center) {
-                    center.insert(rule.center, HashMap::new());
-                }
-
-                let right = center.get_mut(rule.center).unwrap();
-                if !right.contains_key(rule.right) {
-                    right.insert(rule.right, rule.next);
-                }
+                config.rules[i] = j;
 
                 next_state
             }
@@ -183,21 +160,27 @@ fn read_file(path: String) -> Result<String, String> {
     Ok(file_content)
 }
 
-fn fired(cells: &Vec<&State>, config: &Config) -> bool {
+fn fired(cells: &Vec<usize>, config: &Config) -> bool {
     for i in 1..cells.len() - 1 {
-        if cells[i].class != config.firing.class {
+        if cells[i] != config.firing {
             return false;
         }
     }
     true
 }
 
-fn new_cells<'a>(n: usize, config: &'a Config) -> Vec<&'a State<'a>> {
-    let mut cells: Vec<&'a State<'a>> = vec![config.soldier; n + 2];
+fn new_line<'a>(n: usize, config: &'a Config) -> Vec<usize> {
+    let mut cells: Vec<usize> = vec![config.soldier; n + 2];
     cells[0] = config.external;
     let i = cells.len() - 1;
     cells[i] = config.external;
     return cells;
+}
+
+fn first_line<'a>(n: usize, config: &'a Config) -> Vec<usize> {
+    let mut cells = new_line(n, config);
+    cells[1] = config.general;
+    cells
 }
 
 fn main() {
@@ -218,30 +201,24 @@ fn main() {
 
     let mut config = parse_rule_file(&s);
 
-    for (_, state) in &config.states {
-        match state.class {
-            "general" => config.general = &state,
-            "soldier" => config.soldier = &state,
-            "external" => config.external = &state,
-            "firing" => config.firing = &state,
+    for (i, state) in config.states.iter().enumerate() {
+        match state.class.as_str() {
+            "general" => config.general = i,
+            "soldier" => config.soldier = i,
+            "external" => config.external = i,
+            "firing" => config.firing = i,
             _ => {}
         }
     }
 
-    let mut env = Env { config: &config };
-
-    let mut current = new_cells(cell_size, &config);
-    let mut next = new_cells(cell_size, &config);
-
-    current[1] = config.general;
+    let mut cells = first_line(cell_size, &config);
 
     let start = Instant::now();
-    // env.dump(&current);
+    dump(&cells, &config);
 
-    while !fired(&current, &config) {
-        env.next_lines(&current, &mut next);
-        (current, next) = (next, current);
-        // env.dump(&current);
+    while !fired(&cells, &config) {
+        cells = nextline(&cells, &config);
+        dump(&cells, &config);
     }
 
     let end = start.elapsed();
